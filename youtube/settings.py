2d9556e7f1e5d6a07029af7b33da205309bbe11f
SatamSai <sainam7740@gmail.com>
2020-12-18 20:39:00 +0530

    xyz

--- youtube/settings.py
+++ youtube/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -131,4 +130,3 @@ STATICFILES_DIRS = [
 ]
 
 YOUTUBE_DATA_API_KEY='AIzaSyBE4RZwX1KGujHUOF0QtbSS1N0FUaK5jLA'
-django_heroku.settings(locals())
